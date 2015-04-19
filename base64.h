cf8dcdb8a4e874c77f3e9a8e9b643e8c17b19131
Felix Fietkau <nbd@openwrt.org>
2015-04-19 12:42:10 +0200

    add build flag to use libubox instead of provided base64 implementation
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- base64.h
+++ base64.h
@@ -1,6 +1,9 @@
 #ifndef __BASE64_H
 #define __BASE64_H
 
+#ifdef USE_LIBUBOX
+#include <libubox/utils.h>
+#else
 int b64_encode(const void *src, size_t src_len,
 	       void *dest, size_t dest_len);
 
@@ -8,5 +11,6 @@ int b64_decode(const void *src, void *dest, size_t dest_len);
 
 #define B64_ENCODE_LEN(_len)	((((_len) + 2) / 3) * 4 + 1)
 #define B64_DECODE_LEN(_len)	(((_len) / 4) * 3 + 1)
+#endif /* USE_LIBUBOX */
 
 #endif
